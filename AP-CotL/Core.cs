6e6238b113dff339e364a3955998c5c17813829c
ReinaldiCotL <reinaldi.cotl@gmail.com>
2024-11-26 00:37:01 -0500

    Change ownership in MelonInfo

--- AP-CotL/Core.cs
+++ AP-CotL/Core.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(AP_CotL.Core), "AP-CotL", "1.0.0", "William", null)]
+[assembly: MelonInfo(typeof(AP_CotL.Core), "AP-CotL", "1.0.0", "ReinaldiCotL", null)]
 [assembly: MelonGame("Massive Monster", "Cult Of The Lamb")]
 
 namespace AP_CotL
