ae8a8810b33729b3dd6720505198c81382c646dc
ReinaldiCotL <reinaldi.cotl@gmail.com>
2024-11-27 10:05:25 -0500

    Create ItemsManager and migrate some logic to it

---
+++ AP-CotL/Archipelago/ItemsManager.cs
@@ -0,0 +1,12 @@
+public class ItemsManager
+{
+    public enum Types
+    {
+        UNSET = -1,
+    }
+
+    public static void Unlock(Types type)
+    {
+        // Unlock logic here.
+    }
+}
--- AP-CotL/Archipelago/Location.cs
+++ AP-CotL/Archipelago/Location.cs
@@ -1,10 +1,5 @@
 public class Location
 {
-    public enum Type
-    {
-        UNSET = -1,
-    }
-
     public int id;
     public Type type;
 }
