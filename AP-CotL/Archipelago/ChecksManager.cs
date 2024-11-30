453a9e3bbd6db95658513d5ad0e763f2060b9eb7
ReinaldiCotL <reinaldi.cotl@gmail.com>
2024-11-30 17:30:36 -0500

    Add check finds

--- AP-CotL/Archipelago/ChecksManager.cs
+++ AP-CotL/Archipelago/ChecksManager.cs
@@ -3,4 +3,38 @@ using static LocationsManager;
 public class ChecksManager
 {
     public static List<Check> checks = new List<Check>();
+
+    public static Check FindCheckByLocation(LocationsManager.Location location)
+    {
+        Check the_check = null;
+
+        foreach(Check check in checks)
+        {
+            if (check.location.Equals(location))
+            {
+                the_check = check;
+                break;
+            }
+        }
+
+        // Check not found.
+        return the_check;
+    }
+
+    public static Check FindCheckByItem(ItemsManager.Item item)
+    {
+        Check the_check = null;
+
+        foreach (Check check in checks)
+        {
+            if (check.item.Equals(item))
+            {
+                the_check = check;
+                break;
+            }
+        }
+
+        // Check not found.
+        return the_check;
+    }
 }
