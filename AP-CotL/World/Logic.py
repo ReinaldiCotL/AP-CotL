4486db81fa3f398273462f63ac1ae9d0ee10fb14
Rachel Sandler <rachel.sandler@talentselect.ai>
2024-12-08 22:06:49 -0500

    Removed worlds files

--- AP-CotL/World/Logic.py
+++
@@ -1,6 +0,0 @@
-#Placeholder
-def has_Anura_Access(state, player):
-    return (state.can_reach("Anura", "Region", player))
-
-def has_Temple(state, player):
-    return (state.has("Temple", player))
